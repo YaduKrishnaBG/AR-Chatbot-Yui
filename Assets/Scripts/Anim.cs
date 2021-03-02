fc26c44c7229b6419bd38727a23513f58e2aa3d5
Yadukrishna <32412602+YaduKrishnaBG@users.noreply.github.com>
2021-03-01 22:13:49 -0500

    First commit

---
+++ Assets/Scripts/Anim.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Anim : MonoBehaviour
+{
+    public Animator anime;
+    public ExampleStreaming exampleStreaming;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anime = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (exampleStreaming.flag == "greet")
+        {
+            anime.Play("female_greet");
+        }
+        if (exampleStreaming.flag == "happy")
+        {
+            anime.Play("female_emotion_happy");
+        }
+
+        if (exampleStreaming.flag == "yes")
+        {
+            anime.Play("female_say_yes");
+        }
+
+    }
+}
